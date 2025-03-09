49cddb648773e4958c26b1e4f65ec3e09154b283
Vosty17 <stephwafula70@gmail.com>
2025-03-09 14:36:24 +0300

    deburg

--- intro-ass.py
+++ intro-ass.py
@@ -1,16 +1,22 @@
-def calculator()
-# Get user input
-num1=float(input("Enter the first number:"))  
-num2=float(input("Enter the second number:"))
-operation=input("Enter the operation (+,-,*,/)")
-#Perform the choosen operation 
-if operation =='+':
-    result=num1+num2
-elif operation=='-':
-    result=num1-num2
-elif operation=='*':
-    result=num1*num2
-elif operation=='/':
-    result=num1/num2
-else:
-    result="Invalid operation"
+def calculator():
+    # Get user input
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    # Perform the chosen operation
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        result = num1 / num2
+    else:
+        result = "Invalid operation"
+
+    # Print the result
+    print(f"{num1}{operation}{num2}={result}")
+#call the calculator function 
+calculator()
